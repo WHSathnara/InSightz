35aa85de156dc9030018408bf6d7d031eda579f6
WH Sathnara <10818161@students.plymouth.ac.uk>
2024-02-01 01:05:55 +0530

    Nav bar and Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-   <h1>hello</h1>
-    </div>
+   <>
+
+        <Router>
+        <Navbar />
+        <Home/>
+        <Routes>
+       
+          <Route path='/' exact  />
+          
+          </Routes>
+      </Router>
+
+  </>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css' ;
+
+
+function Home() {
+  return (
+    <>
+      
+      <div className='home'>
+      <h1>Welcome to In-Sightz</h1>
+      <p>Unlock the power of unified insights with In-Sightz! One platform, eight social media channels. Effortlessly track, analyze, and optimize your posts. Receive personalized suggestions, schedule with ease, and stay ahead of trends. Perfect for content creators, businesses, and talent showcasing their best. Elevate your social media game -  join In-Sightz today!</p>
+      <button className='butn--outline'>Sign-Up</button>
+    </div>
+    </>
+  );
+}
+
+export default Home;
